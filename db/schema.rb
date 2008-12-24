7087c5309e974ed77810626197a7edf82c9ad395
Robert Verrill <verrill@verrill-laptop.(none)>
2008-12-24 23:04:38 +0000

    peformed the following steps ruby script/generate scaffold course name:string
    git add app/controllers/courses_controller.rb
    app/helpers/courses_helper.rb app/models/course.rb app/views/courses/
    app/views/layouts/courses.html.erb
    db/migrate/20081224230207_create_courses.rb test/fixtures/courses.yml
    test/functional/courses_controller_test.rb test/unit/course_test.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081224225603) do
+ActiveRecord::Schema.define(:version => 20081224230207) do
 
   create_table "attendees", :force => true do |t|
     t.integer  "lecture_id", :null => false
     t.integer  "person_id",  :null => false
     t.boolean  "examined"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "courses", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
