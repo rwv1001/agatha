bc99c5723ad4a5618f0079c7c42a53fe3235183f
Agatha App <agathaapp@verrill-laptop.(none)>
2009-01-09 20:25:16 +0000

    added files after capify

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+require 'mongrel_cluster/recipes'
+
+default_run_options[:pty] = true
+set :application, "agatha"
+set :repository,  "agathaapp@fido.bfriars.ox.ac.uk:/opt/repos/agatha.git"
+set :domain, "fido.bfriars.ox.ac.uk"
+
+set :mongrel_conf, "#[current_path]/config/mongrel_cluster.yml"
+set :deploy_to, "/opt/httpd/html/#{application}"
+
+
+set :scm, :git
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :user, "agathaapp"
+set :runner, "agathaapp"
+set :use_sudo, false
+ssh_options[:paranoid] = false
+
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+# moves over server config files after deploying the code
+task :update_config, :roles => [:app] do
+  run "cp -Rf #{shared_path}/cached-copy/config/* #{release_path}/config/"
+end
+after 'deploy:update_code', :update_config
+
