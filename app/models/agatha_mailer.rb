c0c614eedb4f07f83dd7ebe64f9e51b01ba91301
agathaapp <agathaapp@blac-agatha.(none)>
2009-10-18 22:43:12 +0100

    angle brackets

--- app/models/agatha_mailer.rb
+++ app/models/agatha_mailer.rb
@@ -3,7 +3,7 @@ class AgathaMailer < ActionMailer::Base
   def email(agatha_email, to_email)
     subject    agatha_email.subject
     recipients to_email
-    from       'robert.verrill@yahoo.co.uk'
+    from       "<#{agatha_email.from_email}>"
     sent_on    Time.now    
     body       :body_text => agatha_email.body
   end
