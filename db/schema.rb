fe8b160bdb90a42420bcdfc7fc3a715ab33ce1b7
agathaapp <agathaapp@blac-agatha.(none)>
2010-05-09 23:33:38 +0100

    it didn't update mark so trying again

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100509202656) do
+ActiveRecord::Schema.define(:version => 20100509223124) do
 
   create_table "agatha_emails", :force => true do |t|
     t.string   "from_email",        :default => ""
